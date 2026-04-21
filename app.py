226c2fdd0c991e072ade9200d070c9d1a18adcee
JanRoessel <roessel.jan@gmail.com>
2026-04-21 08:07:26 +0200

    added html files

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ def write_posts(posts):
     with open(POSTS_FILE, 'w') as f:
         json.dump(posts, f, indent=4)
 
+def fetch_post_by_id(post_id):
+    posts = read_posts()
+    for post in posts:
+        if post['id'] == post_id:
+            return post
+    return None
+
 @app.route('/')
 def index():
     blog_posts = read_posts()
@@ -40,6 +47,25 @@ def delete(post_id):
     posts = [post for post in posts if post['id'] != post_id]
     write_posts(posts)
     return redirect(url_for('index'))
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        posts = read_posts()
+        for p in posts:
+            if p['id'] == post_id:
+                p['author'] = request.form.get('author')
+                p['title'] = request.form.get('title')
+                p['content'] = request.form.get('content')
+                break
+        write_posts(posts)
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
